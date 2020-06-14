8ffead68301f181eec31ae285c6abe707e50b84b
Steven Woerpel <woerpels@uwplatt.edu>
2020-06-13 20:17:58 -0500

    added default params object

--- frontend/src/services/knight_trap_weave.service.ts
+++ frontend/src/services/knight_trap_weave.service.ts
@@ -11,6 +11,73 @@ class KnightTrapWeaveService {
         {rows:16,cols:16},
         {rows:32,cols:32},
     ];
+
+    public defaultParams = {
+        trap_count: 4,
+        canvas: {
+            "width": 800,
+            "height": 800
+        },
+        grid: {
+            "cols": this.grid_sizes[0].cols,
+            "rows": this.grid_sizes[0].rows,
+            "max_value": 4,
+            "max_value_step": 1,
+            "increment_max_value": true,
+            "random": true
+        },
+        weave:{
+            return {
+                "queue_length": 5,
+                "smooth":{
+                    "iter_start": 0,
+                    "iter_end": 8,
+                    "ratio":0.25
+                },
+                "on": true,
+                "alpha": 1,
+                "width":{
+                    "dynamic": true,
+                    "init":  0.5,
+                    "min": 0.25,
+                    "max": 1,
+                    "step": 0.25,
+                    "oss_freq": 1
+                },
+                "border":{
+                    "on": false,
+                    "width":0.1,
+                    "color": "black"
+                }
+            }
+        },
+        knight: {
+            "init":{
+                "mode": "start",
+                "x": 18,
+                "y": 18
+            },
+            "jump":{
+                "x":1,
+                "y":1
+            },
+            "on": false,
+            "mode": "squares",
+            "alpha": 1,
+            "border":{
+                "on": false,
+                "width":0.0,
+                "color": "black",
+                "alpha":1
+            }
+        },
+        color:{
+            "domain":12,
+            "palette": 'Spectral',
+            "background": "light-grey"
+        }
+    }
+
     constructor() {}
 
     paramFactory(options = {grid_size_index:0,color_palette:'Spectral'}){
